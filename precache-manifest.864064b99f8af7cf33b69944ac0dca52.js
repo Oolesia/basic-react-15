8b0012da402b3adfb632adba11bc75d8b2397ac2
Olesia Sikora <sikora0310@gmail.com>
2020-08-19 23:08:18 +0300

    Updates

---
+++ precache-manifest.864064b99f8af7cf33b69944ac0dca52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a4bbaea355ae021489d508d74f2cbd",
+    "url": "/basic-react-15/index.html"
+  },
+  {
+    "revision": "fa64c45c821aae2f8b47",
+    "url": "/basic-react-15/static/css/main.786ea6a2.chunk.css"
+  },
+  {
+    "revision": "01b2b9bbe4607a973940",
+    "url": "/basic-react-15/static/js/2.ef1775d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic-react-15/static/js/2.ef1775d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa64c45c821aae2f8b47",
+    "url": "/basic-react-15/static/js/main.8344ea10.chunk.js"
+  },
+  {
+    "revision": "7a0ec8f72f562d4fc67e",
+    "url": "/basic-react-15/static/js/runtime-main.5611f64a.js"
+  }
+]);
